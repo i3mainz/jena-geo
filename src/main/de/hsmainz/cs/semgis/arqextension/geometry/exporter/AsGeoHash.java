ed9c7101b2449f785744523d775fec71268d21fb
Timo Homburg <timo.homburg@hs-mainz.de>
2019-09-28 21:36:19 +0200

    Changed pom and raster algebra

--- src/main/de/hsmainz/cs/semgis/arqextension/geometry/exporter/AsGeoHash.java
+++ src/main/de/hsmainz/cs/semgis/arqextension/geometry/exporter/AsGeoHash.java
@@ -4,8 +4,10 @@ import java.math.BigInteger;
 
 import org.apache.jena.sparql.expr.NodeValue;
 import org.apache.jena.sparql.function.FunctionBase2;
-import org.apache.sis.index.GeoHashCoder;
+import org.apache.sis.referencing.CommonCRS;
+import org.apache.sis.referencing.gazetteer.GeohashReferenceSystem;
 import org.locationtech.jts.algorithm.Angle;
+import org.opengis.referencing.operation.TransformException;
 
 import io.github.galbiston.geosparql_jena.implementation.GeometryWrapper;
 
@@ -14,18 +16,26 @@ import io.github.galbiston.geosparql_jena.implementation.GeometryWrapper;
  *
  */
 public class AsGeoHash extends FunctionBase2 {
-
-	GeoHashCoder coder=new GeoHashCoder();
 	
 	@Override
 	public NodeValue exec(NodeValue v1, NodeValue v2) {
 		GeometryWrapper geom1 = GeometryWrapper.extract(v1);
 		BigInteger maxchars=v2.getInteger();
-		if(geom1.getGeometryType().equalsIgnoreCase("Point")) {
-			String geohash = coder.encode(Angle.toDegrees(geom1.getXYGeometry().getCoordinate().getX()), Angle.toDegrees(geom1.getXYGeometry().getCoordinate().getY()));
-			return NodeValue.makeString(geohash);
+		GeohashReferenceSystem refsys;
+		try {
+			refsys = new GeohashReferenceSystem(GeohashReferenceSystem.Format.BASE32, CommonCRS.WGS84.normalizedGeographic());
+			GeohashReferenceSystem.Coder coder=refsys.createCoder();
+			if(geom1.getGeometryType().equalsIgnoreCase("Point")) {
+				String geohash = coder.encode(Angle.toDegrees(geom1.getXYGeometry().getCoordinate().getX()), Angle.toDegrees(geom1.getXYGeometry().getCoordinate().getY()));
+				return NodeValue.makeString(geohash);
+			}
+			throw new RuntimeException("Input geometry needs to be a Point");
+		} catch (TransformException e) {
+			// TODO Auto-generated catch block
+			throw new RuntimeException(e.getMessage());
 		}
-		throw new RuntimeException("Input geometry needs to be a Point");
+
+		
 	}
 
 }
--- src/main/de/hsmainz/cs/semgis/arqextension/raster/algebra/Div.java
+++ src/main/de/hsmainz/cs/semgis/arqextension/raster/algebra/Div.java
@@ -1,14 +1,20 @@
 package de.hsmainz.cs.semgis.arqextension.raster.algebra;
 
+import java.awt.image.BufferedImage;
+import java.awt.image.DataBuffer;
+import java.awt.image.WritableRaster;
 import java.awt.image.renderable.ParameterBlock;
+import java.util.LinkedList;
+import java.util.List;
 
 import javax.media.jai.JAI;
 import javax.media.jai.RenderedOp;
 
 import org.apache.jena.sparql.expr.NodeValue;
 import org.apache.jena.sparql.function.FunctionBase2;
+import org.apache.sis.coverage.SampleDimension;
 import org.apache.sis.coverage.grid.GridCoverage;
-import org.opengis.coverage.Coverage;
+import org.apache.sis.internal.coverage.BufferedGridCoverage;
 
 import io.github.galbiston.geosparql_jena.implementation.CoverageWrapper;
 
@@ -19,15 +25,42 @@ public class Div extends FunctionBase2  {
 		CoverageWrapper wrapper=CoverageWrapper.extract(v1);
 		GridCoverage raster=wrapper.getXYGeometry();
 		CoverageWrapper wrapper2=CoverageWrapper.extract(v2);
-		GridCoverage raster2=wrapper.getXYGeometry();
+		GridCoverage raster2=wrapper2.getXYGeometry();
+	     Integer rd1 = 0, rd2 = 0;
 		 ParameterBlock pbSubtracted = new ParameterBlock(); 
 	     pbSubtracted.addSource(raster.render(raster.getGridGeometry().getExtent())); 
 	     pbSubtracted.addSource(raster2.render(raster2.getGridGeometry().getExtent())); 
 	     RenderedOp subtractedImage = JAI.create("divide",pbSubtracted);
-	     GridCoverageFactory gridCoverageFactory = new GridCoverageFactory(); 
-	     Envelope referencedEnvelope = new ReferencedEnvelope(REDC.getEnvelope()); 
-	        Coverage coverage = gridCoverageFactory.create("Raster", NDVIop, REDC.getEnvelope()); 
-	     return CoverageWrapper.createGeometry(subtractedImage.getAsBufferedImage(), wrapper.getSrsURI(), wrapper.getRasterDatatypeURI()).asNodeValue();
+			/*
+			 * final GridGeometry grid = new
+			 * GridGeometry(raster.getGridGeometry().getExtent(), PixelInCell.CELL_CENTER,
+			 * MathTransforms.identity(2),
+			 * raster.getGridGeometry().getCoordinateReferenceSystem());
+			 * 
+			 * final MathTransform1D toUnits = (MathTransform1D) MathTransforms.linear(0.5,
+			 * 100);
+			 */
+			final SampleDimension sd = new SampleDimension.Builder().setName("t")
+					.addQuantitative(
+							(raster.getSampleDimensions().get(rd1).getName() + " / "
+									+ raster2.getSampleDimensions().get(rd2).getName()).toString(),
+							raster.getSampleDimensions().get(0).getMeasurementRange().get(),
+							raster.getSampleDimensions().get(0).getTransferFunction().get(),
+							raster.getSampleDimensions().get(0).getUnits().get())
+					.build();
+			
+			List<SampleDimension>sds=new LinkedList<SampleDimension>();
+			sds.add(sd);
+			/*
+			 * Create the grid coverage, gets its image and set values directly as short
+			 * integers.
+			 */
+			BufferedGridCoverage coverage = new BufferedGridCoverage(raster2.getGridGeometry(),
+					sds, DataBuffer.TYPE_SHORT);
+			WritableRaster rasterr = ((BufferedImage) coverage.render(null)).getRaster();
+			rasterr.setRect(subtractedImage.getSourceImage(0).getData());
+			return CoverageWrapper.createGeometry(coverage, wrapper.getSrsURI(), wrapper.getRasterDatatypeURI())
+					.asNodeValue();	     
 
 	     
 	} 
