bb0716495b9ff599884772b849cbae04a47da529
timo.homburg <timo.homburg@hs-mainz.de>
2020-11-04 23:22:34 +0100

    Union fix

--- src/main/de/hsmainz/cs/semgis/arqextension/geometry/relation/Union.java
+++ src/main/de/hsmainz/cs/semgis/arqextension/geometry/relation/Union.java
@@ -4,29 +4,62 @@ import org.apache.jena.datatypes.DatatypeFormatException;
 import org.apache.jena.sparql.expr.ExprEvalException;
 import org.apache.jena.sparql.expr.NodeValue;
 import org.apache.jena.sparql.function.FunctionBase2;
+import org.apache.sis.geometry.Envelope2D;
+import org.geotoolkit.coverage.grid.GridCoverage2D;
 import org.locationtech.jts.geom.Geometry;
 import org.opengis.geometry.MismatchedDimensionException;
 import org.opengis.referencing.operation.TransformException;
 import org.opengis.util.FactoryException;
 
+import de.hsmainz.cs.semgis.arqextension.util.LiteralUtils;
+import de.hsmainz.cs.semgis.arqextension.util.Wrapper;
 import io.github.galbiston.geosparql_jena.implementation.GeometryWrapper;
 import io.github.galbiston.geosparql_jena.implementation.GeometryWrapperFactory;
+import io.github.galbiston.geosparql_jena.implementation.datatype.WKTDatatype;
+import io.github.galbiston.geosparql_jena.implementation.datatype.raster.CoverageWrapper;
 
 public class Union extends FunctionBase2 {
 
 	@Override
 	public NodeValue exec(NodeValue v1, NodeValue v2) {
-		try {
-            GeometryWrapper geometry = GeometryWrapper.extract(v1);
-            Geometry geom = geometry.getXYGeometry();
-            GeometryWrapper geometry2 = GeometryWrapper.extract(v2);
-            GeometryWrapper transGeom2 = geometry2.transform(geometry.getSRID());
-            Geometry uniongeom=geom.union(transGeom2.getXYGeometry());
-            GeometryWrapper unionWrapper = GeometryWrapperFactory.createGeometry(uniongeom, geometry.getSrsURI(), geometry.getGeometryDatatypeURI());
-            return unionWrapper.asNodeValue();
-        } catch (DatatypeFormatException | MismatchedDimensionException | TransformException | FactoryException ex) {
-            throw new ExprEvalException(ex.getMessage(), ex);
-        }
+		Wrapper wrapper1=LiteralUtils.rasterOrVector(v1);
+		Wrapper wrapper2=LiteralUtils.rasterOrVector(v2);
+		if(wrapper1 instanceof GeometryWrapper && wrapper2 instanceof GeometryWrapper) {
+			try {
+	            GeometryWrapper geometry = GeometryWrapper.extract(v1);
+	            Geometry geom = geometry.getXYGeometry();
+	            GeometryWrapper geometry2 = GeometryWrapper.extract(v2);
+	            GeometryWrapper transGeom2 = geometry2.transform(geometry.getSRID());
+	            Geometry uniongeom=geom.union(transGeom2.getXYGeometry());
+	            GeometryWrapper unionWrapper = GeometryWrapperFactory.createGeometry(uniongeom, geometry.getSrsURI(), geometry.getGeometryDatatypeURI());
+	            return unionWrapper.asNodeValue();
+	        } catch (DatatypeFormatException | MismatchedDimensionException | TransformException | FactoryException ex) {
+	            throw new ExprEvalException(ex.getMessage(), ex);
+	        }
+		}else if(wrapper1 instanceof CoverageWrapper && wrapper2 instanceof CoverageWrapper) {
+			GridCoverage2D raster=((CoverageWrapper)wrapper1).getXYGeometry();
+			GridCoverage2D raster2=((CoverageWrapper)wrapper2).getXYGeometry();	
+			Envelope2D uniongeom=raster.getEnvelope2D().createUnion(raster2.getEnvelope2D());
+	        GeometryWrapper unionWrapper = GeometryWrapperFactory.createGeometry(LiteralUtils.toGeometry(uniongeom), WKTDatatype.URI);
+	        return unionWrapper.asNodeValue();		
+		}else {
+			if(wrapper1 instanceof CoverageWrapper) {
+				GridCoverage2D raster=((CoverageWrapper)wrapper1).getXYGeometry();
+				org.opengis.geometry.Envelope bbox1 = raster.getGridGeometry().getEnvelope();
+				Geometry geom=((GeometryWrapper)wrapper2).getXYGeometry();
+				Geometry uniongeom=geom.union(LiteralUtils.toGeometry(raster.getGridGeometry().getEnvelope()));
+		        GeometryWrapper unionWrapper = GeometryWrapperFactory.createGeometry(uniongeom, WKTDatatype.URI);
+		        return unionWrapper.asNodeValue();		
+			}else {
+				GridCoverage2D raster=((CoverageWrapper)wrapper2).getXYGeometry();
+				org.opengis.geometry.Envelope bbox1 = raster.getGridGeometry().getEnvelope();
+				Geometry geom=((GeometryWrapper)wrapper1).getXYGeometry();
+				Geometry uniongeom=geom.union(LiteralUtils.toGeometry(raster.getGridGeometry().getEnvelope()));
+		        GeometryWrapper unionWrapper = GeometryWrapperFactory.createGeometry(uniongeom, WKTDatatype.URI);
+		        return unionWrapper.asNodeValue();			
+			}
+		}
+
 	}
 
 }
